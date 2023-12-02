f3289edbbf51f01453c89eb18f2cea25237ce262
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-12-02 02:25:17 +0100

    Make generic AOC

--- default/main.go
+++ default/main.go
@@ -1,37 +1,37 @@
 package main
- 
+
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
- 
+
 func main() {
- 
+
 	// read file
-    readFile, err := os.Open(`C:\Users\info\Desktop\audite\code\go\adventofcode\1\input.txt`)
+	readFile, err := os.Open(`.\1\input.txt`)
 
 	// if error
-    if err != nil {
-        fmt.Println(err)
-    }
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// scan file
-    fileScanner := bufio.NewScanner(readFile)
+	fileScanner := bufio.NewScanner(readFile)
 
 	// split file
-    fileScanner.Split(bufio.ScanLines)
+	fileScanner.Split(bufio.ScanLines)
 
 	// for each line
-    for fileScanner.Scan() {
+	for fileScanner.Scan() {
 
 		// read line
 		line := fileScanner.Text()
-		
+
 		// print line
 		fmt.Println(line)
-    }
-  
+	}
+
 	// close file
-    readFile.Close()
+	readFile.Close()
 }
