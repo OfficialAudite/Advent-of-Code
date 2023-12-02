a05f36cb79274b3c3077b8e7fd421f7e66a10618
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-12-02 02:00:24 +0100

    Default File

---
+++ default/main.go
@@ -0,0 +1,37 @@
+package main
+ 
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+ 
+func main() {
+ 
+	// read file
+    readFile, err := os.Open(`C:\Users\info\Desktop\audite\code\go\adventofcode\1\input.txt`)
+
+	// if error
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	// scan file
+    fileScanner := bufio.NewScanner(readFile)
+
+	// split file
+    fileScanner.Split(bufio.ScanLines)
+
+	// for each line
+    for fileScanner.Scan() {
+
+		// read line
+		line := fileScanner.Text()
+		
+		// print line
+		fmt.Println(line)
+    }
+  
+	// close file
+    readFile.Close()
+}
